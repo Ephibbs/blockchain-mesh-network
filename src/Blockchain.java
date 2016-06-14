56f92e704b5b00a8747f8902ed415d1973ba8f6e
Evan Phibbs <evan.phibbs@gmail.com>
2016-06-14 10:54:40 -0700

    added Blockchain and Node classes empty

---
+++ src/Blockchain.java
@@ -0,0 +1,9 @@
+/*
+* Blockchain.java
+* An implementation of blockchain
+ */
+public class Blockchain {
+    public Blockchain() {
+
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,6 @@
+/**
+ * Node.java
+ * represents a node in a network
+ */
+public class Node {
+}
