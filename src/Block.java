63d3c335b1579fffa50d83d87f846a6798c91e32
Evan Phibbs <evan.phibbs@gmail.com>
2016-06-15 11:57:27 -0700

    implemented block

---
+++ src/Block.java
@@ -0,0 +1,23 @@
+/**
+ * Created by evan on 6/15/16.
+ */
+public class Block {
+    private String myHash;
+    private String prevHash;
+    private Message[] msgs;
+    private String nonce;
+    Block(){
+
+    }
+    void setMyHash(myHash) {
+        this.myHash = myHash;
+    }
+
+    void setPrevHash(prevHash) {
+        this.prevHash = prevHash;
+    }
+
+    void addMessage(msg) {
+        ;
+    }
+}
