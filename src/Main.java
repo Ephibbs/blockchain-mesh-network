abfeb293caa0d619eea837411ceaee687b852405
stullam <583403@bah.com>
2016-06-16 14:13:07 -0400

    Updated Main and Node Class
    This has comments in it and an improved method for message distribution
    and friend addition

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,26 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
+	// These nodes are available nodes in the global network.
 	public Node node1, node2, node3, node4, node5, node6, node7, node8,node9;
 	public ArrayList<Node> networkNodes = new ArrayList<Node>();
-	public ArrayList<Message> globalMSG = new ArrayList<Message>();
 	
+	/*
+	 * This function is to basically just give each node a name and add the
+	 * nodes to an arraylist to similar the network
+	 */
 	public Main() {
-		node1 = new Node("Steve", this);
-		node2 = new Node("Damian", this);
-		node3 = new Node("Lawrence", this);
-		node4 = new Node("Jeffrey", this);
-		node5 = new Node("Evan", this);
-		node6 = new Node("Natalie", this);
-		node7 = new Node("Will", this);
-		node8 = new Node("Dylan", this);
-		node9 = new Node("Colby", this);
+		node1 = new Node("Steve");
+		node2 = new Node("Damian");
+		node3 = new Node("Lawrence");
+		node4 = new Node("Jeffrey");
+		node5 = new Node("Evan");
+		node6 = new Node("Natalie");
+		node7 = new Node("Will");
+		node8 = new Node("Dylan");
+		node9 = new Node("Colby");
 		
+		//adding the nodes to the "network"
 		networkNodes.add(node1);
 		networkNodes.add(node2);
 		networkNodes.add(node3);
@@ -27,46 +32,56 @@ public class Main {
 		networkNodes.add(node8);
 		networkNodes.add(node9);
 		
-		//generateCommunicationLines();
+		//This is a method call to the generate communication lines method
+		generateCommunicationLines();
 	}
-	
+	/*
+	 * This method is for giving each node the other nodes that it can talk to.
+	 * Doing it this way allows us to simulate a distribution because each node
+	 * can only talk to a maximum of n/2 nodes.
+	 */
 	private void generateCommunicationLines() {
-		// TODO Auto-generated method stub
-		for(int i = 0; i <networkNodes.size();i++){
-			Node nodeToGetFriends = networkNodes.get(i);
-			Random rand = new Random();
-			int numberOfFriends = rand.nextInt(networkNodes.size());
+		// Do not worry about this part it is just defining a class so i can use
+		// it later
+		Random rand = new Random();
+		
+		// just defining the number of friends that each node can have
+		Double n = (double) networkNodes.size();
+		Double maxFriendsDouble = Math.ceil(n/2);
+		int maxFriends = maxFriendsDouble.intValue();
+		
+		//this first forloop is only to make sure that each node gets friends to talk to
+		for(int i = 0; i < n.intValue();i++){
+			Node nodeToGiveFriends = networkNodes.get(i);
+			ArrayList<Node> possibleFriends = (ArrayList<Node>) networkNodes.clone();
+			possibleFriends.remove(i);
 			
-			
-//			if(this.nodeID.equals(networkNodes.get(receiverNum))){
-//				this.createMessage(data);
-//				System.out.println("I reran the function");
-//			}
-//			else {
-//				Message text = new TextMessage("hello ", networkNodes.get(receiverNum));
-//				localMSG.add(text);
-//				main.addMSG(text);
-//			}
+			// This next for loop is the one that actually gives nodes their friends
+			// it goes through and makes sure you dont give the same node more then 2 friends
+			for(int j = 0; j < maxFriends-1; j++) {
+				int randomFriend = rand.nextInt(possibleFriends.size());
+				nodeToGiveFriends.addFriend(possibleFriends.get(randomFriend));
+				possibleFriends.remove(randomFriend);
+			}
+		}
+	}
+
+	/*
+	 * This method is to just test run making messages
+	 */
+	public void run() {
+		node1.createMessage("I like tacos");
+		
+		System.out.println("A list of all of node 1's friends");
+		for(int i = 0; i < node1.networkNodes.size();i++){
+			System.out.println("Friend" + (1+i) + " is: " + node1.networkNodes.get(i).getNodeID());
 		}
 		
-	}
-
-	public void run() {
-		node1.createMessage("I like me");
-	}
-	
-	public void addMSG(Message msg) {
-		this.globalMSG.add(msg);
-		this.verifyMessageDistribution();
-	}
-	private void verifyMessageDistribution() {
-		for(int i = 0; i < networkNodes.size();i++) {
-			networkNodes.get(i).verifyMessages();
+		System.out.println("A list of who all has a message: ");
+		for(int j = 0; j < this.networkNodes.size();j++) {
+			if(this.networkNodes.get(j).localMSG.size()>0) {
+				System.out.println("I have a message: " + this.networkNodes.get(j).getNodeID());
+			}
 		}
 	}
-
-	public ArrayList<Message> getGlobalMSG() {
-		return this.globalMSG;
-	}
-
 }
--- src/Node.java
+++ src/Node.java
@@ -2,60 +2,64 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Node {
-	
+
 	public String nodeID = null;
 	public ArrayList<Message> localMSG = new ArrayList<Message>();
 	public ArrayList<Node> networkNodes = new ArrayList<Node>();
-	public Main main;
-	
+
+	// These are the constructors for the node class
 	public Node(String id) {
 		this.nodeID = id;
 	}
 
-	public Node(String id, Main main) {
-		this.nodeID = id;
-		this.main = main;
-	}
-
+	// This allows other classes to get the name of the node
 	public String getNodeID() {
 		return nodeID;
 	}
-	
-	public void addNodes(ArrayList<Node> newNodes){
-		for(int i = 0; i < newNodes.size(); i++) {
+
+	// This is one mechanism so we are able to add friends to nodes
+	// this one is a group of friends at a time.
+	public void addNodes(ArrayList<Node> newNodes) {
+		for (int i = 0; i < newNodes.size(); i++) {
 			networkNodes.add(newNodes.get(i));
 		}
 	}
-	
-	public void printNodes(){
-		for(int i = 0; i < networkNodes.size(); i++) {
+
+	// This is another mechanism so we are able to add friends to nodes
+	// this is one friend at a time
+	public void addFriend(Node node) {
+		networkNodes.add(node);
+	}
+
+	// This prints the nodes that each node is friends with
+	public void printNodes() {
+		for (int i = 0; i < networkNodes.size(); i++) {
 			System.out.println(networkNodes.get(i).getNodeID());
 		}
 	}
-	
+
+	// This is the method for creating a message and adding a random recipient
+	// from a nodes friend list. The message is then distribute to this nodes
+	// friend
 	public void createMessage(Object data) {
 		Random rand = new Random();
-		int receiverNum = rand.nextInt(4);
-		if(this.nodeID.equals(networkNodes.get(receiverNum))){
-			this.createMessage(data);
-			System.out.println("I reran the function");
-		}
-		else {
-			Message text = new TextMessage("hello ", networkNodes.get(receiverNum));
-			localMSG.add(text);
-			main.addMSG(text);
+		int receiverNum = rand.nextInt(networkNodes.size());
+
+		Message text = new TextMessage("hello ", networkNodes.get(receiverNum));
+
+		localMSG.add(text);
+		this.distributeMessage(text);
+	}
+
+	// THis is how the message is distributed to its friends
+	private void distributeMessage(Message text) {
+		for (int i = 0; i < networkNodes.size(); i++) {
+			networkNodes.get(i).addMessage(text);
 		}
 	}
 
-	public void verifyMessages() {
-		for(int i = 0; i < main.getGlobalMSG().size();i++) {
-			if(!localMSG.contains(main.getGlobalMSG().get(i))){
-				localMSG.add(main.getGlobalMSG().get(i));
-				System.out.println("I updated " + this.nodeID + " local message bank");
-			}
-			else {
-				// do nothing
-			}
-		}
+	// Adding a message to a nodes list of messages it has received
+	private void addMessage(Message text) {
+		this.localMSG.add(text);
 	}
 }
