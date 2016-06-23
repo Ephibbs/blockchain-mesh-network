1f3212cabc6cb4cce07ae6ce4fd1375b473f4f86
stullam <583403@bah.com>
2016-06-23 09:36:41 -0700

    Network GUI Works in the Basic Case

---
+++ src/NetworkGUI.java
@@ -0,0 +1,221 @@
+
+/* 
+ * File: FacePamphlet.java
+ * -----------------------
+ * When it is finished, this program will implement a basic social network
+ * management system.
+ */
+
+import acm.program.*;
+import acm.graphics.*;
+import acm.util.*;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.*;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.*;
+
+public class NetworkGUI extends Program {
+
+	/** Number of characters for each of the text input fields */
+	public static final int TEXT_FIELD_SIZE = 15;
+
+	/* Private instance variables */
+	public JTextField diffString;
+	public JTextField numNodes;
+	public JTextField commRadString;
+	public JTextField Send;
+	public JTextField Receive;
+	public JTextField messageText;
+	public JTextField randMessage;
+
+	public ArrayList<Node> networkNodes = new ArrayList<Node>();
+
+	public int nodeIDCounter = 0;
+	public int difficulty = 1;
+	public int numberOfNodes = 10;
+	public int communicationRadius = 10;
+	public int MAXSIZE = 800;
+	public int OFFSET = 15;
+
+	public Canvas canvas = new Canvas();
+	public Random rand = new Random();
+	public Graphics g = this.canvas.getGraphics();
+
+	/**
+	 * This method has the responsibility for initializing the interactors in
+	 * the application, and taking care of any other initialization that needs
+	 * to be performed.
+	 */
+	@Override
+	public void init() {
+		this.setSize(new Dimension(1100, 850));
+
+		// fields on the West Side of the screen
+		generateWestFrame();
+
+		// Action listeners
+		addActionListeners();
+		addListeners();
+
+		add(this.canvas);
+	}
+
+	private void addListeners() {
+		this.diffString.addActionListener(this);
+		this.numNodes.addActionListener(this);
+		this.commRadString.addActionListener(this);
+		this.randMessage.addActionListener(this);
+		this.messageText.addActionListener(this);
+	}
+
+	private void generateWestFrame() {
+		add(new JLabel("Enter the Hash Difficulty below"), WEST);
+		this.diffString = new JTextField(TEXT_FIELD_SIZE);
+		add(this.diffString, WEST);
+		add(new JButton("Difficulty"), WEST);
+
+		add(new JLabel("Number of Nodes in Network Below"), WEST); // space
+																	// holder
+		this.numNodes = new JTextField(TEXT_FIELD_SIZE);
+		add(this.numNodes, WEST);
+		add(new JButton("Number of Nodes"), WEST);
+
+		add(new JLabel("Communication Radius Below"), WEST); // space holder
+		this.commRadString = new JTextField(TEXT_FIELD_SIZE);
+		add(this.commRadString, WEST);
+		add(new JButton("Comm radius"), WEST);
+
+		add(new JButton("Generate Network Graphic"), WEST);
+
+		// Joining a group
+		add(new JLabel("Enter Message Below"), WEST); // space holder
+		this.messageText = new JTextField(TEXT_FIELD_SIZE);
+		add(this.messageText, WEST);
+		this.Send = new JTextField(TEXT_FIELD_SIZE);
+		this.Receive = new JTextField(TEXT_FIELD_SIZE);
+		add(new JLabel("Enter Sender Below"), WEST);
+		add(this.Send, WEST);
+		add(new JLabel("Enter Receiver Below"), WEST);
+		add(this.Receive, WEST);
+		add(new JButton("Send Message"), WEST);
+
+		// Creating a new Group
+		add(new JLabel("Random Message Below"), WEST); // space holder
+		this.randMessage = new JTextField(TEXT_FIELD_SIZE);
+		add(this.randMessage, WEST);
+		add(new JButton("Random Message"), WEST);
+	}
+
+	/**
+	 * This class is responsible for detecting when the buttons are clicked or
+	 * interactors are used, so you will have to add code to respond to these
+	 * actions.
+	 */
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Lookup button is clicked
+		if (e.getActionCommand().equals("Difficulty") && !this.diffString.getText().equals("")) {
+			this.difficulty = Integer.parseInt(this.diffString.getText());
+			System.out.println("Your difficulty is: " + this.difficulty);
+		}
+		// Change Status is clicked or user clicked enter after entering a
+		// status in the text field
+		else if (e.getActionCommand().equals("Number of Nodes")
+				|| e.getSource() == this.numNodes && !this.numNodes.getText().equals("")) {
+			this.numberOfNodes = Integer.parseInt(this.numNodes.getText());
+			System.out.println("Your numberOfNodes is: " + this.numberOfNodes);
+
+		}
+		// Change Picture is clicked or user clicked enter after entering
+		// picture name into the text field
+		else if (e.getActionCommand().equals("Comm radius")
+				|| e.getSource() == this.commRadString && !this.commRadString.getText().equals("")) {
+			System.out.println("Radius is as big as your mom");// extractButtonFunctionality();
+			this.communicationRadius = Integer.parseInt(this.commRadString.getText());
+			System.out.println("Your communication radius is: " + this.communicationRadius);
+		}
+		// Add Friend is clicked or user clicked enter after entering a friends
+		// name into the text field
+		else if (e.getActionCommand().equals("Send Message")
+				|| e.getSource() == this.messageText && !this.messageText.getText().equals("")) {
+			// System.out.println("Send
+			// Message");//addFriendFunctionality(enteredName);
+			try {
+				sendMessage(this.messageText.getText(), this.Send.getText(), this.Receive.getText());
+			} catch (NoSuchAlgorithmException | NoSuchProviderException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		// Create a group is clicked or user clicked enter after entering a
+		// group name into the text field
+		else if (e.getActionCommand().equals("Random Message")
+				|| e.getSource() == this.randMessage && !this.randMessage.getText().equals("")) {
+			System.out.println("You are a Random");// createGroupFunctionality();
+		} else if (e.getActionCommand().equals("Generate Network Graphic")) {
+			// generateNodeNetwork();
+			try {
+				generateNodes();
+			} catch (NoSuchAlgorithmException e1) {
+				e1.printStackTrace();
+			} catch (NoSuchProviderException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	private void sendMessage(String message, String sender, String receiver) throws NoSuchAlgorithmException, NoSuchProviderException {
+		Node senderNode = null;
+		Node receiverNode = null;
+		for (int i = 0; i < networkNodes.size(); i++) {
+			for (int j = 0; j < networkNodes.size(); j++) {
+				if (networkNodes.get(i).nodeID.equals(sender) && networkNodes.get(j).nodeID.equals(receiver)) {
+					senderNode = networkNodes.get(i);
+					receiverNode = networkNodes.get(j);
+					senderNode.setNodeValues(senderNode.getXCoord(), senderNode.getYCoord(), 
+							Color.RED, senderNode.getWidth());
+					senderNode.Draw(g);
+					receiverNode.setNodeValues(receiverNode.getXCoord(), receiverNode.getYCoord(), 
+							Color.RED, receiverNode.getWidth());
+					receiverNode.Draw(g);
+				}
+			}
+		}
+	}
+
+	private void generateNodes() throws NoSuchAlgorithmException, NoSuchProviderException {
+		Node n;
+		this.g = this.canvas.getGraphics();
+		for (int i = 0; i < this.numberOfNodes; i++) {
+			int randomX = rand.nextInt(MAXSIZE - OFFSET) + OFFSET / 2;
+			int randomY = rand.nextInt(MAXSIZE - OFFSET) + OFFSET / 2;
+			n = new Node("Node" + nodeIDCounter);
+			n.setBlockChainDifficulty(this.difficulty);
+			n.setNodeValues(randomX, randomY, Color.BLUE, OFFSET);
+			networkNodes.add(n);
+			this.nodeIDCounter++;
+			n.Draw(g);
+		}
+	}
+
+	private void generateNodeNetwork() {
+		Node n;
+		Graphics g = this.canvas.getGraphics();
+		for (int i = 0; i < this.numberOfNodes; i++) {
+			int randomX = rand.nextInt(MAXSIZE - OFFSET) + OFFSET / 2;
+			int randomY = rand.nextInt(MAXSIZE - OFFSET) + OFFSET / 2;
+			g.setColor(Color.BLUE);
+			g.fillOval(randomX, randomY, OFFSET, OFFSET);
+		}
+	}
+
+}
--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Graphics;
 import java.io.Serializable;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -39,18 +41,31 @@ public class Node implements Serializable {
 	public byte[] byteArray = new byte[1024];
 	public Blockchain blockChain = new Blockchain(this);
 
+	public int xCoordinate = 0;
+	public int yCoordinate = 0;
+	public Color color = Color.BLUE;
+	public int WIDTH = 0;
+
 	// Constructor
 	public Node(String id) throws NoSuchAlgorithmException, NoSuchProviderException {
-		
+
 		this.nodeID = id;
-		this.keyGen = KeyPairGenerator.getInstance("DSA", "SUN"); // create key generator object
-		this.random = SecureRandom.getInstance("SHA1PRNG", "SUN"); // random var for random generation
-		this.dsa = Signature.getInstance("SHA1withDSA", "SUN"); // create signature object
-		keyGen.initialize(1024, random); // initialize and generate random key pair
+		this.keyGen = KeyPairGenerator.getInstance("DSA", "SUN"); // create key
+																	// generator
+																	// object
+		this.random = SecureRandom.getInstance("SHA1PRNG", "SUN"); // random var
+																	// for
+																	// random
+																	// generation
+		this.dsa = Signature.getInstance("SHA1withDSA", "SUN"); // create
+																// signature
+																// object
+		keyGen.initialize(1024, random); // initialize and generate random key
+											// pair
 		this.pair = keyGen.generateKeyPair();
 		this.privKey = pair.getPrivate();
 		this.pubKey = pair.getPublic();
-		//this.blockChain = new Blockchain(this);
+		// this.blockChain = new Blockchain(this);
 		// distributePublicKey()
 	}
 
@@ -58,6 +73,7 @@ public class Node implements Serializable {
 	public String getNodeID() {
 		return nodeID;
 	}
+
 	public PublicKey getPublicKey() {
 		return this.pubKey;
 	}
@@ -65,28 +81,40 @@ public class Node implements Serializable {
 	// Mutators
 	public void createMessage(Object data) {
 
-		distributePublicKey(this.getPublicKey()); // distribute public key to friend nodes (they will propagate it to their friends)
+		distributePublicKey(this.getPublicKey()); // distribute public key to
+													// friend nodes (they will
+													// propagate it to their
+													// friends)
 		System.out.println("I distributed my public key");
 
-		Random rand = new Random(); // create a message with a random friend node as the recipient
+		Random rand = new Random(); // create a message with a random friend
+									// node as the recipient
 		int receiverNum = rand.nextInt(networkNodes.size());
 		Message text = new TextMessage(data, this, networkNodes.get(receiverNum));
-		
+
 		this.blockChain.add(text);
 
 		localMSG.add(text);
-		this.distributeMessage(text); // distribute message to friend nodes (they will propagate to their friends)
+		this.distributeMessage(text); // distribute message to friend nodes
+										// (they will propagate to their
+										// friends)
 	}
-	public void createMessageWithSignature(Object data) throws InvalidKeyException, SignatureException, NoSuchAlgorithmException, NoSuchProviderException {
+
+	public void createMessageWithSignature(Object data)
+			throws InvalidKeyException, SignatureException, NoSuchAlgorithmException, NoSuchProviderException {
 
 		// distributePublicKey(this.getPublicKey());
 		System.out.println("I distributed my public key");
 
-		Random rand = new Random(); // create a message with a random friend node as the recipient
+		Random rand = new Random(); // create a message with a random friend
+									// node as the recipient
 		int receiverNum = rand.nextInt(networkNodes.size());
 		Message text = new TextMessage(data, this, networkNodes.get(receiverNum));
 
-		byteArray = text.getMessageData().toString().getBytes(); // convert message to series of bytes
+		byteArray = text.getMessageData().toString().getBytes(); // convert
+																	// message
+																	// to series
+																	// of bytes
 		byte[] realSig = new byte[1024];
 
 		dsa.initSign(this.privKey); // sign message with private key
@@ -94,18 +122,29 @@ public class Node implements Serializable {
 		realSig = dsa.sign();
 
 		localMSG.add(text);
-		this.distributeSignedMessage(realSig, byteArray, text); // distribute message to friend nodes (they will propagate to their friends)
+		this.distributeSignedMessage(realSig, byteArray, text); // distribute
+																// message to
+																// friend nodes
+																// (they will
+																// propagate to
+																// their
+																// friends)
 	}
-	public void addNodes(ArrayList<Node> newNodes) { // add a group of friend nodes
+
+	public void addNodes(ArrayList<Node> newNodes) { // add a group of friend
+														// nodes
 		for (int i = 0; i < newNodes.size(); i++) {
 			networkNodes.add(newNodes.get(i));
 		}
 	}
+
 	public void addFriend(Node node) { // add a single friend node
 		networkNodes.add(node);
 	}
+
 	public void addMessage(Message text) {
-		if (this.localMSG.contains(text)) { // if message is unique, add and distribute
+		if (this.localMSG.contains(text)) { // if message is unique, add and
+											// distribute
 			// do nothing
 		} else {
 			this.blockChain.add(text);
@@ -113,17 +152,22 @@ public class Node implements Serializable {
 			this.distributeMessage(text);
 		}
 	}
-	public void addSignedMessage(byte[] realSig, byte[] byteArray2, Message text) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
+
+	public void addSignedMessage(byte[] realSig, byte[] byteArray2, Message text)
+			throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
 		Signature sig = Signature.getInstance("SHA1withDSA", "SUN");
-		for (int i = 0; i < publicKeySet.size(); i++) { // find public key that can verify message
+		for (int i = 0; i < publicKeySet.size(); i++) { // find public key that
+														// can verify message
 			sig.initVerify(publicKeySet.get(i));
 			sig.update(byteArray2);
 			boolean verifies = sig.verify(realSig);
-			//System.out.println("signature verifies: " + verifies);
-			//System.out.println("the public key I verified with was: " + publicKeySet.get(i).toString());
-			//System.out.println("I am node: " + this.getNodeID());
+			// System.out.println("signature verifies: " + verifies);
+			// System.out.println("the public key I verified with was: " +
+			// publicKeySet.get(i).toString());
+			// System.out.println("I am node: " + this.getNodeID());
 
-			if (verifies == true) { // if verified and unique, add to localMSG and distribute friend nodes
+			if (verifies == true) { // if verified and unique, add to localMSG
+									// and distribute friend nodes
 				if (!this.localMSG.contains(text)) {
 					localMSG.add(text);
 					this.distributeSignedMessage(realSig, byteArray2, text);
@@ -136,34 +180,52 @@ public class Node implements Serializable {
 			}
 		}
 	}
+
 	public void addPublicKey(PublicKey publicKey) {
 		// TODO Auto-generated method stub
-		if (this.publicKeySet.contains(publicKey)) { // if public key is unique, add and distribute to friend nodes
+		if (this.publicKeySet.contains(publicKey)) { // if public key is unique,
+														// add and distribute to
+														// friend nodes
 			// do nothing
 		} else {
 			this.publicKeySet.add(publicKey);
 			this.distributePublicKey(publicKey);
 		}
 	}
+
 	public void distributeMessage(Message text) {
-		for (int i = 0; i < networkNodes.size(); i++) { // distribute message to friend nodes (they will propagate to their friends)
+		for (int i = 0; i < networkNodes.size(); i++) { // distribute message to
+														// friend nodes (they
+														// will propagate to
+														// their friends)
 			networkNodes.get(i).addMessage(text);
 		}
 	}
-	public void distributeSignedMessage(byte[] realSig, byte[] byteArray2, Message text) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
+
+	public void distributeSignedMessage(byte[] realSig, byte[] byteArray2, Message text)
+			throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < networkNodes.size(); i++) { // give each friend node the signed message, saved in their localMSG
+		for (int i = 0; i < networkNodes.size(); i++) { // give each friend node
+														// the signed message,
+														// saved in their
+														// localMSG
 			networkNodes.get(i).addSignedMessage(realSig, byteArray2, text);
 		}
 	}
+
 	public void distributePublicKey(PublicKey publicKey) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < networkNodes.size(); i++) { // add public key to all friend nodes (they will propagate to their friends)
+		for (int i = 0; i < networkNodes.size(); i++) { // add public key to all
+														// friend nodes (they
+														// will propagate to
+														// their friends)
 			networkNodes.get(i).addPublicKey(publicKey);
 		}
 	}
+
 	public void distributeBlock(Block b) {
-		for (int i = 0; i < networkNodes.size(); i++) { // add block to friend node's blockchain
+		for (int i = 0; i < networkNodes.size(); i++) { // add block to friend
+														// node's blockchain
 			networkNodes.get(i).blockChain.add(b);
 		}
 	}
@@ -173,16 +235,46 @@ public class Node implements Serializable {
 		System.out.println(nodeID);
 		blockChain.start();
 	}
+
 	public void printNodes() {
-		for (int i = 0; i < networkNodes.size(); i++) { // print out friend nodes
+		for (int i = 0; i < networkNodes.size(); i++) { // print out friend
+														// nodes
 			System.out.println(networkNodes.get(i).getNodeID());
 		}
 	}
+
 	public boolean isOnline() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
 	public void setBlockChainDifficulty(int difficulty) {
 		blockChain.setDifficulty(difficulty);
+	}
+	
+	public void setNodeValues(int xVal, int yVal, Color myColor, int width)
+			throws NoSuchAlgorithmException, NoSuchProviderException {
+		this.xCoordinate = xVal;
+		this.yCoordinate = yVal;
+		this.color = myColor;
+		this.WIDTH = width;
+	}
+
+	public void Draw(Graphics g) {
+		g.setColor(this.color);
+		g.fillOval(this.xCoordinate, this.yCoordinate, this.WIDTH, this.WIDTH);
+	}
+	
+	public Color getColor(){
+		return this.color;
+	}
+	public int getXCoord(){
+		return this.xCoordinate;
+	}
+	public int getYCoord(){
+		return this.yCoordinate;
+	}
+	public int getWidth() {
+		return this.WIDTH;
 	}
 }
